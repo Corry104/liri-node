28326c56ef00e42606b8d351e47f3c275316110d
Jack <Jack@Corrados-MacBook-Pro.local>
2018-12-19 00:27:27 -0800

    changes

--- Liri.js
+++ Liri.js
@@ -3,6 +3,25 @@
 
 var Spotify = require('node-spotify-api');
 
-require("dotenv").config();
-
+var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
+var moment = require("moment");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var movieTitle = "titanic";
+var artistName = "Maroon 5";
+
+axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy").then(
+    function(response) {
+    //   console.log(response.data);
+    }
+   );
+
+axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").then(
+    function(response) {
+    console.log(response.data[0].lineup[0]);
+    console.log(response.data[0].venue.name);
+    console.log(response.data[0].datetime);
+    }
+   );
+      
